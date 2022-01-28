df18e4f8270a56e2d3b308c009f82e060ec3098e
Matthew Casperson <matthewcasperson@gmail.com>
2022-01-28 15:49:59 +1000

    Added other methods

--- src/main/java/com/matthewcasperson/azuretranslate/Function.java
+++ src/main/java/com/matthewcasperson/azuretranslate/Function.java
@@ -30,7 +30,7 @@ public class Function {
      * https://github.com.cnpmjs.org/microsoft/azure-maven-plugins/issues/1351
      */
     @FunctionName("transcribe")
-    public HttpResponseMessage run(
+    public HttpResponseMessage transcribe(
             @HttpTrigger(
                 name = "req",
                 methods = {HttpMethod.POST},
@@ -53,6 +53,63 @@ public class Function {
         } catch (final IOException | ExecutionException | InterruptedException ex) {
             return request.createResponseBuilder(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body("There was an error transcribing the audio file.")
+                .build();
+        }
+    }
+
+    @FunctionName("translate")
+    public HttpResponseMessage translate(
+        @HttpTrigger(
+            name = "req",
+            methods = {HttpMethod.POST},
+            authLevel = AuthorizationLevel.ANONYMOUS)
+        HttpRequestMessage<Optional<String>> request,
+        final ExecutionContext context) {
+
+        if (request.getBody().isEmpty()) {
+            return request.createResponseBuilder(HttpStatus.BAD_REQUEST)
+                .body("The text to translate file must be in the body of the post.")
+                .build();
+        }
+
+        try {
+            final String text = TRANSLATE_SERVICE.translate(
+                request.getBody().get(),
+                request.getQueryParameters().get("sourceLanguage"),
+                request.getQueryParameters().get("targetLanguage"));
+
+            return request.createResponseBuilder(HttpStatus.OK).body(text).build();
+        } catch (final IOException ex) {
+            return request.createResponseBuilder(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body("There was an error translating the text.")
+                .build();
+        }
+    }
+
+    @FunctionName("speak")
+    public HttpResponseMessage speak(
+        @HttpTrigger(
+            name = "req",
+            methods = {HttpMethod.POST},
+            authLevel = AuthorizationLevel.ANONYMOUS)
+        HttpRequestMessage<Optional<String>> request,
+        final ExecutionContext context) {
+
+        if (request.getBody().isEmpty()) {
+            return request.createResponseBuilder(HttpStatus.BAD_REQUEST)
+                .body("The text to translate file must be in the body of the post.")
+                .build();
+        }
+
+        try {
+            final byte[] audio = SPEECH_SERVICE.translateText(
+                request.getBody().get(),
+                request.getQueryParameters().get("targetLanguage"));
+
+            return request.createResponseBuilder(HttpStatus.OK).body(audio).build();
+        } catch (final IOException ex) {
+            return request.createResponseBuilder(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body("There was an error converting the text to speech.")
                 .build();
         }
     }
